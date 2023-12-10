d4f5eb1efa990aa6cf06dfaf9189fdd25cde1aa0
krid <krid123435@hotmail.com>
2023-12-10 22:23:01 +0700

    delete out folder

--- clock-angle.js
+++ clock-angle.js
@@ -3,22 +3,15 @@ function getClockAngle(x) {
     var min = Number(x.split(":")[1]);
     var hrAg = hr <= 12 ? hr * 30 + min / 2 : (hr - 12) * 30 + min / 2;
     var minAg = min * 6;
-    console.log(hrAg, minAg);
     var ans = 0;
-    console.log(ans);
     ans += hrAg - minAg;
-    console.log(typeof ans);
     if (ans >= 180) {
-        console.log("ans too much");
         ans = ans - 180;
-        console.log(ans);
     }
     else if (ans <= 0) {
-        console.log("ans is negative");
         ans = 0 - ans;
-        console.log(ans);
     }
-    console.log(ans);
-    return 10;
+    console.log("ans", ans);
+    return ans;
 }
 getClockAngle("16:52");
--- out/clock-angle.js
+++
@@ -1,8 +0,0 @@
-function getClockAngle(x) {
-    var hr = x.split(":")[0];
-    var min = x.split(":")[1];
-    console.log(hr, min);
-    return 10;
-}
-getClockAngle("krid");
-//# sourceMappingURL=clock-angle.js.map
